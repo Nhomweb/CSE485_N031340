f50d7a3940f5e7093a73e759c3c9ca8f0c37b1ad
nguyennhat1998 <japan26081998@gmail.com>
2018-12-20 00:59:42 +0700

    commit

---
+++ N031340/sources/admin/includes/side-bar.php
@@ -0,0 +1,62 @@
+<div class="nav-side-menu">
+    <div class="brand">Admin Panel</div>
+    <i class="fa fa-bars fa-3x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+
+    <div class="menu-list">
+
+        <ul id="menu-content" class="menu-content collapse out nav">
+
+            <li>
+                <a href="index.php">
+                    <i class="fa fa-dashboard fa-lg "></i> Dashboard
+                </a>
+            </li>
+
+            <li>
+                <a href="users.php">
+                    <i class="fa fa-users fa-lg"></i> Users
+                </a>
+            </li>
+
+         
+
+            <li data-toggle="collapse" data-target="#service" class="collapsed">
+                <a href="#"><i class="fa fa-spinner fa-lg"></i> Thể loại <span class="arrow"></span></a>
+            </li>
+            <ul class="sub-menu collapse out" id="service">
+                <li>New Service 1</li>
+                <li>New Service 2</li>
+                <li>New Service 3</li>
+            </ul>
+
+
+            <li data-toggle="collapse" data-target="#new" class="collapsed">
+                <a href="#"><i class="fa fa-gamepad  fa-lg"></i>Game<span class="arrow"></span></a>
+            </li>
+            <ul class="sub-menu collapse out" id="new">
+                <li>New New 1</li>
+                <li>New New 2</li>
+                <li>New New 3</li>
+            </ul>
+
+            <li>
+                <a href="#">
+                    <i class="fa fa-comments-o  fa-lg"></i> Bình luận
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <i class="fa fa-ra fa-lg"></i> Quảng cáo
+                </a>
+            </li>
+            
+            <li>
+                <a href="functions/log-out.php">
+                    <i class="fa fa-user fa-lg"></i> Log out
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</div>
